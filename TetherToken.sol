9e6c30ac27306467436246a2d2fd887003614666
حليم <285200070+hlme7788145-a11y@users.noreply.github.com>
2026-05-28 22:45:06 +0300

    Refactor TetherToken contract with updated structure

--- TetherToken.sol
+++ TetherToken.sol
@@ -1,6 +1,24 @@
 pragma solidity ^0.4.17;
 event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 address public pendingOwner;
+"linkReferences": {
+    "remix_accounts.sol": {
+        "TestsAccounts": [
+            { "length": 20, "start": 987 }
+        ]
+    },
+    "remix_tests.sol": {
+        "Assert": [
+            { "length": 20, "start": 268 },
+            { "length": 20, "start": 442 },
+            { "length": 20, "start": 616 },
+            { "length": 20, "start": 793 },
+            { "length": 20, "start": 901 },
+            { "length": 20, "start": 957 },
+            { "length": 20, "start": 1291 }
+        ]
+    }
+}ماذا يوجد اشرح لي هاذا بختصلر 
 /**
  * @title SafeMath
  * @dev Math operations with safety checks that throw on error
