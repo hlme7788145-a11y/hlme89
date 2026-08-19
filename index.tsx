3ccf746ecc0d9c46740537c9dfd20f479c172d54
حليم <285200070+hlme7788145-a11y@users.noreply.github.com>
2026-08-19 04:28:54 +0300

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,65 +1,130 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import translate from 'translations';
+import { Token } from 'types/network';
 import { AppState } from 'features/reducers';
-import { walletSelectors } from 'features/wallet';
-import EquivalentValues from './EquivalentValues';
-import AccountInfo from './AccountInfo';
-import Promos from './Promos';
-import TokenBalances from './TokenBalances';
-
-interface Block {
-  name: string;
-  content: React.ReactElement<any>;
-  isFullWidth?: boolean;
-}
+import * as selectors from 'features/selectors';
+import { configSelectors, configMetaSelectors } from 'features/config';
+import { customTokensActions } from 'features/customTokens';
+import { walletTypes, walletActions, walletSelectors } from 'features/wallet';
+import Spinner from 'components/ui/Spinner';
+import Balances from './Balances';
+import './index.scss';
 
 interface StateProps {
   wallet: AppState['wallet']['inst'];
+  walletConfig: AppState['wallet']['config'];
+  tokens: Token[];
+  tokenBalances: walletTypes.TokenBalance[];
+  tokensError: AppState['wallet']['tokensError'];
+  isTokensLoading: AppState['wallet']['isTokensLoading'];
+  hasSavedWalletTokens: AppState['wallet']['hasSavedWalletTokens'];
+  isOffline: AppState['config']['meta']['offline'];
 }
+interface ActionProps {
+  attemptAddCustomToken: customTokensActions.TAttemptAddCustomToken;
+  removeCustomToken: customTokensActions.TRemoveCustomToken;
+  scanWalletForTokens: walletActions.TScanWalletForTokens;
+  setWalletTokens: walletActions.TSetWalletTokens;
+  refreshTokenBalances: walletActions.TRefreshTokenBalances;
+}
+type Props = StateProps & ActionProps;
 
-export class BalanceSidebar extends React.Component<StateProps> {
+class TokenBalances extends React.Component<Props> {
   public render() {
-    const { wallet } = this.props;
+    const {
+      tokens,
+      walletConfig,
+      tokenBalances,
+      hasSavedWalletTokens,
+      isTokensLoading,
+      tokensError,
+      isOffline
+    } = this.props;
 
-    if (!wallet) {
-      return null;
+    const walletTokens = walletConfig ? walletConfig.tokens : [];
+
+    let content;
+    if (tokensError) {
+      content = (
+        <div className="TokenBalances-error well well-md">
+          <h5 className="TokenBalances-error-message">{tokensError}</h5>
+          <button onClick={this.props.refreshTokenBalances} className="btn btn-default btn-sm">
+            {translate('X_TRY_AGAIN')}
+            <i className="fa fa-refresh" />
+          </button>
+        </div>
+      );
+    } else if (isTokensLoading) {
+      content = (
+        <div className="TokenBalances-loader">
+          <Spinner size="x3" />
+        </div>
+      );
+    } else if (!walletTokens && !isOffline) {
+      content = (
+        <button
+          className="TokenBalances-scan btn btn-primary btn-block"
+          onClick={this.scanWalletForTokens}
+        >
+          {translate('SCAN_TOKENS')}
+        </button>
+      );
+    } else {
+      const shownBalances =
+        (walletTokens && tokenBalances.filter(t => walletTokens.includes(t.symbol))) || [];
+
+      content = (
+        <Balances
+          allTokens={tokens}
+          tokenBalances={shownBalances}
+          hasSavedWalletTokens={hasSavedWalletTokens}
+          scanWalletForTokens={this.scanWalletForTokens}
+          setWalletTokens={this.props.setWalletTokens}
+          onAddCustomToken={this.props.attemptAddCustomToken}
+          onRemoveCustomToken={this.props.removeCustomToken}
+          isOffline={isOffline}
+        />
+      );
     }
 
-    const blocks: Block[] = [
-      {
-        name: 'Account Info',
-        content: <AccountInfo wallet={wallet} />
-      },
-      {
-        name: 'Promos',
-        isFullWidth: true,
-        content: <Promos />
-      },
-      {
-        name: 'Token Balances',
-        content: <TokenBalances />
-      },
-      {
-        name: 'Equivalent Values',
-        content: <EquivalentValues />
-      }
-    ];
-
     return (
-      <aside>
-        {blocks.map(block => (
-          <section className={`Block ${block.isFullWidth ? 'is-full-width' : ''}`} key={block.name}>
-            {block.content}
-          </section>
-        ))}
-      </aside>
+      <section className="TokenBalances">
+        <h5 className="TokenBalances-title">{translate('SIDEBAR_TOKENBAL')}</h5>
+        {content}
+      </section>
     );
   }
+
+  private scanWalletForTokens = () => {
+    if (this.props.wallet) {
+      this.props.scanWalletForTokens(this.props.wallet);
+      this.setState({ hasScanned: true });
+    }
+  };
 }
 
-const mapStateToProps = (state: AppState): StateProps => ({
-  wallet: walletSelectors.getWalletInst(state)
-});
+function mapStateToProps(state: AppState): StateProps {
+  return {
+    wallet: walletSelectors.getWalletInst(state),
+    walletConfig: walletSelectors.getWalletConfig(state),
+    tokens: configSelectors.getAllTokens(state),
+    tokenBalances: selectors.getTokenBalances(state),
+    tokensError: state.wallet.tokensError,
+    isTokensLoading: state.wallet.isTokensLoading,
+    hasSavedWalletTokens: state.wallet.hasSavedWalletTokens,
+    isOffline: configMetaSelectors.getOffline(state)
+  };
+}
 
-export default connect(mapStateToProps)(BalanceSidebar);
+export default connect(
+  mapStateToProps,
+  {
+    attemptAddCustomToken: customTokensActions.attemptAddCustomToken,
+    removeCustomToken: customTokensActions.removeCustomToken,
+    scanWalletForTokens: walletActions.scanWalletForTokens,
+    setWalletTokens: walletActions.setWalletTokens,
+    refreshTokenBalances: walletActions.refreshTokenBalances
+  }
+)(TokenBalances);
